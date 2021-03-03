66362d6a0364f9cd8b9f514f0a95e71102ddd26e
stm32wrobotyce <78742706+stm32wrobotyce@users.noreply.github.com>
2021-03-03 09:57:20 +0100

    Add files via upload

--- Core/Src/pid_controller.c
+++ Core/Src/pid_controller.c
@@ -9,7 +9,7 @@
 
 void pid_init(pid_str *pid_data, float kp_init, float ki_init, float kd_init, int anti_windup_limit_init)
 {
-	pid_data->previous_variable = 0;
+	pid_data->previous_error = 0;
 	pid_data->total_error = 0;
 
 	pid_data->Kp = kp_init;
@@ -22,7 +22,7 @@ void pid_init(pid_str *pid_data, float kp_init, float ki_init, float kd_init, in
 void pid_reset(pid_str *pid_data)
 {
 	pid_data->total_error = 0;
-	pid_data->previous_variable = 0;
+	pid_data->previous_error = 0;
 }
 
 int pid_calculate(pid_str *pid_data, int setpoint, int process_variable)
@@ -35,12 +35,12 @@ int pid_calculate(pid_str *pid_data, int setpoint, int process_variable)
 
 	p_term = (float)(pid_data->Kp * error);													//odpowiedź członu proporcjonalnego
 	i_term = (float)(pid_data->Ki * pid_data->total_error);									//odpowiedź członu całkującego
-	d_term = (float)(pid_data->Kd * (process_variable - pid_data->previous_variable));		//odpowiedź członu różniczkującego
+	d_term = (float)(pid_data->Kd * (error - pid_data->previous_error));		//odpowiedź członu różniczkującego
 
 	if(i_term >= pid_data->anti_windup_limit) i_term = pid_data->anti_windup_limit;			//Anti-Windup - ograniczenie odpowiedzi członu całkującego
 	else if(i_term <= -pid_data->anti_windup_limit) i_term = -pid_data->anti_windup_limit;
 
-	pid_data->previous_variable = process_variable;											//aktualizacja zmiennej z poprzednią wartością zmierzoną
+	pid_data->previous_error = error;											//aktualizacja zmiennej z poprzednią wartością błędu
 
 	return (int)(p_term + i_term + d_term);														//odpowiedź regulatora
 }
